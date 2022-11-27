0e6147d40a4b332a66e5c6063ef987c8af33f648
lilyyang <1592218619@qq.com>
2022-11-27 23:03:39 +0800

    Money.tsx 之 备注 UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -27,7 +27,23 @@ const TagsSection = styled.section`
 `
 
 const NotesSection = styled.section`
-
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+  > label {
+    display: flex;
+    align-items: center;
+    > span {
+      margin-right: 16px; white-space: nowrap;
+    }
+    > input{
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
 `
 
 const CategorySection = styled.section`
@@ -58,7 +74,7 @@ function Money() {
           <span>
             备注
           </span>
-          <input type="text" />
+          <input type="text" placeholder='请在这里添加备注'/>
         </label>
       </NotesSection>
       <CategorySection>
