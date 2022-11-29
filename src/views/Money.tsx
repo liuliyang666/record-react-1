9d5446e546428bbe9de6f20e535425de53017fd1
lilyyang <1592218619@qq.com>
2022-11-30 00:24:18 +0800

    收集表单数据

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,22 +5,54 @@ import { TagsSection } from './Money/TagsSection';
 import { NoteSection } from './Money/NoteSection';
 import { CategorySection } from './Money/CategorySection';
 import { NumberPadSection } from './Money/NumberPadSection';
+import {useState} from 'react';
 
 
 
-const MyLayout = styled(Layout)`
-  
+const MyLayout = styled(Layout)`  
   display: flex;
   flex-direction: column;
-`
+`;
 
-function Money() {
+type Category = '-' | '+'
+
+function Money(){
+  const [selected, setSelected] = useState({
+    tags: [] as string[],
+    note: '',
+    category: '-' as Category,
+    amount: 0
+  })
   return (
     <MyLayout>
-      <TagsSection/>        
-      <NoteSection/>
-      <CategorySection/>
-      <NumberPadSection />      
+      <TagsSection value={selected.tags} onChange={(tags) => setSelected({
+        ...selected,
+        tags: tags
+      })}/>        
+      <NoteSection value={selected.note}
+                   onChange={(note) => setSelected({
+                    ...selected,
+                    note: note
+                   })
+                  }
+      />
+      <CategorySection value={selected.category}
+                       onChange={(category)=>{
+                        setSelected({
+                          ...selected,
+                          category: category
+                        })
+                       }}
+      />
+      <NumberPadSection value={selected.amount}
+      onChange={(amount) => {
+        setSelected({
+          ...selected,
+          amount: amount
+        });
+      } }
+      onOk={()=>{}}    
+      />      
     </MyLayout>
   );
 }
