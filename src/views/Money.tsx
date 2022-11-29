0a12796fda3bac50548197cd54bbc8d6d157736a
lilyyang <1592218619@qq.com>
2022-11-29 20:54:37 +0800

    numberPad 功能 wip(work in progress)

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { TagsSection } from './Money/TagsSection';
 import { NoteSection } from './Money/NoteSection';
 import { CategorySection } from './Money/CategorySection';
-import { NumberSection } from './Money/NumberPadSection';
+import { NumberPadSection } from './Money/NumberPadSection';
 
 
 
@@ -17,33 +17,10 @@ const MyLayout = styled(Layout)`
 function Money() {
   return (
     <MyLayout>
-      <TagsSection/>
-        
+      <TagsSection/>        
       <NoteSection/>
       <CategorySection/>
-      <NumberSection>
-        <div className='output'>
-          100
-        </div>
-        <div className="pad clearfix">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>删除</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>清空</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button className='ok'>OK</button>
-          <button className='zero'>0</button>
-          <button className='dot'>.</button>
-          
-        </div>
-      </NumberSection>
-      
+      <NumberPadSection />      
     </MyLayout>
   );
 }
