28aeee31126fe3e332ea938793dbd8fbc63aee5e
lilyyang <1592218619@qq.com>
2022-12-01 16:53:26 +0800

    创建  /tags/:tag 路由，学会 exact 的用法

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { useTags } from '../useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import { Link } from 'react-router-dom';
+
 
 const TagList = styled.ol`
   font-size: 16px;
@@ -10,11 +12,15 @@ const TagList = styled.ol`
   > li{
     border-bottom: 1px solid #d5d5d9;
     line-height: 20px;
-    padding: 12px 16px 12px 0;
+    
     margin-left: 16px;
-    display: flex;
+   
+    > a{
+      padding: 12px 16px 12px 0;
+      display: flex;
     justify-content: space-between;
     align-items: center;
+    }
   }
 `
 
@@ -46,8 +52,11 @@ function Tags() {
         <TagList>
         {tags.map(tag => 
           <li key={tag}>
+            <Link to={'/tags/' + tag}>
             <span className='oneLine'>{tag}</span>
             <Icon name='right'/>
+            </Link>
+            
           </li>  
         )}
         </TagList>
