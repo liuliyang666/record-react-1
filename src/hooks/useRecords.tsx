7b4fd9cb13e36a766b5cb4224b99d560669f9fb5
lilyyang <1592218619@qq.com>
2022-12-05 22:10:27 +0800

    检查输入

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useUpdate } from "./useUpdate";
-
 type RecordItem = {
   tagIds: number[];
   note: string;
@@ -9,7 +8,6 @@ type RecordItem = {
   createdAt: string; // ISO 8601
 };
 type newRecordItem = Omit<RecordItem, "createdAt">;
-
 export const useRecords = () => {
   const [records, setRecords] = useState<RecordItem[]>([]);
   useEffect(() => {
@@ -20,8 +18,17 @@ export const useRecords = () => {
   }, [records]);
 
   const addRecord = (newRecord: newRecordItem) => {
+    if (newRecord.amount <= 0) {
+      alert("请输入金额");
+      return false;
+    }
+    if (newRecord.tagIds.length === 0) {
+      alert("请选择标签");
+      return false;
+    }
     const record = { ...newRecord, createdAt: new Date().toISOString() };
     setRecords([...records, record]);
+    return true;
   };
 
   return { records, addRecord };
